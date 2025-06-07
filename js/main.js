b2612f76a954e88682ba73fe0f61ccaa27910990
AnissaDemirsoy <tl.anissa01@gmail.com>
2025-06-07 20:51:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,14 +11,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Navigation toggle for mobile
-const navBtn = document.getElementById("nav-btn");
-const nav = document.getElementById("nav");
-const navBtnImg = document.getElementById("nav-btn-img");
-
-navBtn.addEventListener("click", () => {
-  if (nav.classList.toggle("open")) {
-    navBtnImg.src = "img/icons/close.svg";
-  } else {
-    navBtnImg.src = "img/icons/open.svg";
-  }
-});
